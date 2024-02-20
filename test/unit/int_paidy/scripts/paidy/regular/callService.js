7d84876802411b9b5ed960cd0b33986dfc24fd47
Sai-LaoKham-Paidy <sai.laokham@paidy.com>
2024-02-20 10:52:33 +0700

    feat(BDPA-1006): downgrade nodejs version to 10.24.1

--- test/unit/int_paidy/scripts/paidy/regular/callService.js
+++ test/unit/int_paidy/scripts/paidy/regular/callService.js
@@ -1,63 +1,63 @@
 /* eslint-disable no-undef */
 'use strict';
 
-var assert = require('chai').assert;
-var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
+// var assert = require('chai').assert;
+// var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 
 describe('SiteGenesis: Try to call service for PAIDY_REGULAR', function () {
-    var serviceRequest = require('../../../../../mocks/serviceRequest');
-    var serviceResponse = require('../../../../../mocks/serviceResponse');
+    // var serviceRequest = require('../../../../../mocks/serviceRequest');
+    // var serviceResponse = require('../../../../../mocks/serviceResponse');
 
-    var CallService = proxyquire('../../../../../../cartridges/int_paidy/cartridge/scripts/paidy/regular/callService', {
-        'dw/svc/LocalServiceRegistry': {
-            createService: function () {
-                return {
-                    URL: null,
-                    call: function () {
-                        return {
-                            ok: true,
-                            object: JSON.stringify(serviceResponse.regularResponse())
-                        };
-                    }
-                };
-            }
-        },
-        'dw/system/Site': {
-            getCurrent: function () {
-                return {
-                    getCustomPreferenceValue: function () {
-                        var params = {
-                            paidy_service_name: 'paidy.api.payment',
-                            paidy_secret_key: 'secret_key'
-                        };
-                        return params.str;
-                    }
-                };
-            }
-        },
-        '*/cartridge/scripts/paidy/regular/paidyLog': {
-            PaidyLog: function () {
-                return {
-                    initLog: function () {
-                        return;
-                    },
-                    writeLog: function () {
-                        return;
-                    }
-                };
-            }
-        }
-    });
+    // var CallService = proxyquire('../../../../../../cartridges/int_paidy/cartridge/scripts/paidy/regular/callService', {
+    //     'dw/svc/LocalServiceRegistry': {
+    //         createService: function () {
+    //             return {
+    //                 URL: null,
+    //                 call: function () {
+    //                     return {
+    //                         ok: true,
+    //                         object: JSON.stringify(serviceResponse.regularResponse())
+    //                     };
+    //                 }
+    //             };
+    //         }
+    //     },
+    //     'dw/system/Site': {
+    //         getCurrent: function () {
+    //             return {
+    //                 getCustomPreferenceValue: function () {
+    //                     var params = {
+    //                         paidy_service_name: 'paidy.api.payment',
+    //                         paidy_secret_key: 'secret_key'
+    //                     };
+    //                     return params.str;
+    //                 }
+    //             };
+    //         }
+    //     },
+    //     '*/cartridge/scripts/paidy/regular/paidyLog': {
+    //         PaidyLog: function () {
+    //             return {
+    //                 initLog: function () {
+    //                     return;
+    //                 },
+    //                 writeLog: function () {
+    //                     return;
+    //                 }
+    //             };
+    //         }
+    //     }
+    // });
 
     // empty definition
-    global.empty = function (params) {
-        if (params) {
-            return false;
-        }
-        return true;
-    };
+    // global.empty = function (params) {
+    //     if (params) {
+    //         return false;
+    //     }
+    //     return true;
+    // };
 
-    // temporarily comment out this test before merging these two PRs 
+    // temporarily comment out this test before merging these two PRs
     // https://github.com/paidy/SalesforceCommerceCloud-AppExchange/pull/7
     // https://github.com/paidy/SalesforceCommerceCloud-AppExchange/pull/6
     // it('should return the response data from the Paidy API', function () {
--- test/unit/int_paidy_sfra/scripts/paidy/paidyHelpers.js
+++ test/unit/int_paidy_sfra/scripts/paidy/paidyHelpers.js
@@ -1,24 +1,24 @@
 /* eslint-disable no-undef */
 'use strict';
 
-var assert = require('chai').assert;
-var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
+// var assert = require('chai').assert;
+// var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 
 describe('SFRA: Check the current payment method', function () {
-    var paidyHelpers = proxyquire('../../../../../cartridges/int_paidy_sfra/cartridge/scripts/paidy/paidyHelpers', {
-        server: function () { return {}; },
-        'dw/system/Transaction': function () { return {}; },
-        'dw/order/PaymentInstrument': function () { return {}; },
-        '*/cartridge/scripts/cart/cartHelpers': function () { return {}; },
-        '*/cartridge/scripts/object/preferences': {
-            PaymentType: {
-                paydyNomal: 'PAIDY_NORMAL',
-                paydyRegular: 'PAIDY_REGULAR'
-            }
-        }
-    });
+    // var paidyHelpers = proxyquire('../../../../../cartridges/int_paidy_sfra/cartridge/scripts/paidy/paidyHelpers', {
+    //     server: function () { return {}; },
+    //     'dw/system/Transaction': function () { return {}; },
+    //     'dw/order/PaymentInstrument': function () { return {}; },
+    //     '*/cartridge/scripts/cart/cartHelpers': function () { return {}; },
+    //     '*/cartridge/scripts/object/preferences': {
+    //         PaymentType: {
+    //             paydyNomal: 'PAIDY_NORMAL',
+    //             paydyRegular: 'PAIDY_REGULAR'
+    //         }
+    //     }
+    // });
 
-    // temporarily comment out these tests before merging these two PRs 
+    // temporarily comment out these tests before merging these two PRs
     // https://github.com/paidy/SalesforceCommerceCloud-AppExchange/pull/7
     // https://github.com/paidy/SalesforceCommerceCloud-AppExchange/pull/6
 
